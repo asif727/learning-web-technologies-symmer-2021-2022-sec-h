65ce0b8bf968592315201ad84cf9a51df6f85352
Asif Chowdhury <106575799+asif727@users.noreply.github.com>
2022-06-08 02:16:58 +0600

    Create qsn3.php

---
+++ qsn3.php
@@ -0,0 +1,13 @@
+<?php
+$number;
+function evenodd($number){
+    if($number%2==0){
+        echo "The number is Even <br>";
+    }
+    else {
+        echo "The number is odd <br>";
+    }
+
+}
+evenodd (15);
+     ?>
