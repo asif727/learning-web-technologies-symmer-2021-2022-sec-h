db0f09b0288cf34a04db27570739e41fc1fb78de
Asif Chowdhury <106575799+asif727@users.noreply.github.com>
2022-06-08 02:17:40 +0600

    Create qsn4.php

---
+++ qsn4.php
@@ -0,0 +1,10 @@
+<?php
+$n1;$n2;$n3;
+function largestNumber($n1,$n2,$n3){
+	if($n1>$n2){
+		if($n1>$n3){echo $n1."is the largest number.<br>";}
+	}
+	elseif($n2>$n3){echo $n2."is the largest number.<br>";}
+}
+largestnumber (50,20,30);
+     ?>
